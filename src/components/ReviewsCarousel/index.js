769ba5706f2a9f1fa1f9d4efcc6a8bccf9c5ef2e
bhanuprakash515 <bhanuprakashinter12@gmail.com>
2023-12-15 12:31:22 +0000

    Reviews App using React JS

--- src/components/ReviewsCarousel/index.js
+++ src/components/ReviewsCarousel/index.js
@@ -4,23 +4,66 @@ import {Component} from 'react'
 import './index.css'
 
 class ReviewsCarousel extends Component {
+  state = {
+    index: 0,
+  }
+
+  onLeftDetails = () => {
+    const {index} = this.state
+
+    if (index <= 0) {
+      this.setState(prevState => ({index: prevState.index}))
+    } else {
+      this.setState(prevState => ({index: prevState.index - 1}))
+    }
+  }
+
+  onRightDetails = () => {
+    const {index} = this.state
+    if (index >= 3) {
+      this.setState(prevState => ({index: prevState.index}))
+    } else {
+      this.setState(prevState => ({index: prevState.index + 1}))
+    }
+  }
+
   render() {
+    const {reviewsList} = this.props
+    const {index} = this.state
+    const {imgUrl, username, companyName, description} = reviewsList[index]
+
     return (
       <div className="container">
         <h1 className="heading">Reviews</h1>
         <div className="card-container">
-          <button className="button" type="button">
+          <button
+            className="button"
+            type="button"
+            onClick={this.onLeftDetails}
+            data-testid="leftArrow"
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png "
+              alt="left arrow"
+            />
+          </button>
+          <div className="image-card-container">
+            <img src={imgUrl} alt={username} />
+            <p className="heading-2">{username}</p>
+            <p className="para">{companyName}</p>
+            <p className="para">{description}</p>
+          </div>
+          <button
+            className="button"
+            type="button"
+            onClick={this.onRightDetails}
+            data-testid="rightArrow"
+          >
             <img
               src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png "
               alt="right arrow"
             />
           </button>
-          <div className="image-card-container">
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/wade-warren-img.png"
-              alt="Wade Warren"
-            />
-          </div>
         </div>
       </div>
     )
